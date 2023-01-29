f309a304d724c07a4e33a378273a75736c2e3f69
kunalagra <git@kunalag.simplelogin.com>
2023-01-30 04:19:34 +0530

    print

--- gcal_app/views.py
+++ gcal_app/views.py
@@ -17,6 +17,7 @@ if not isfile(CLIENT_SECRETS_FILE):
     RED_URL =  'https://gcal-event-basic.onrender.com/rest/v1/calendar/redirect'
 
 print(RED_URL)
+print(CLIENT_SECRETS_FILE)
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection and REDIRECT URL.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile','openid']
